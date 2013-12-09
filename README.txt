3c29f4113943ebe9737e8e45b1aaa44ef9f16513
Huu Nguyen <hknguyen13@gmail.com>
2013-12-09 17:23:37 -0500

    Stealth Project

--- README.txt
+++ README.txt
@@ -1,8 +1,15 @@
 Unity Project Tutorial:
 
+Roll-a-ball:
 Short minigame where the player controls a ball and the goal is to collect all of the cubes.
 https://rawgithub.com/hknguyen13/Unity_Git/master/Roll-a-ball_1/Roll-a-ball_1.html
 
 HAVE FUN!
 
 Tutorial is listed here: http://unity3d.com/learn/tutorials/projects/roll-a-ball
+
+Stealth:
+Short minigame where the player controls Ethan who is trying to escape a facility, via the elevator,
+while avoiding confronting the robot guards. Good Luck!
+
+Tutorial is listed here: http://unity3d.com/learn/tutorials/projects/stealth
---
+++ Stealth_Proj/Stealth/Assets/Scripts/AlarmSystems/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	public float speed;
+	public GUIText countText;
+	public GUIText winText;
+	private int count;
+	
+	void Start() {
+		count = 0;
+		setCountText();
+		winText.text = "";
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		
+		rigidbody.AddForce(movement * speed * Time.deltaTime);
+	}
+	
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive(false);	
+			count++;
+			setCountText();
+		}
+	}
+	
+	void setCountText() {
+		countText.text = "Count: " + count.ToString(); 
+		
+		if (count >= 13) {
+			winText.text = "YOU WIN!";
+		}
+	}
+}
